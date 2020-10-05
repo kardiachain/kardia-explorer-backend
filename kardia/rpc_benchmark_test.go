1c10b72a18c318da83f45b691b0081bcb3d4676a
y4t0g4m1 <dangnhattrinh.cbl@gmail.com>
2020-10-05 22:32:16 +0700

    Edit unit and benchmark test

--- kardia/rpc_benchmark_test.go
+++ kardia/rpc_benchmark_test.go
@@ -19,43 +19,15 @@
 package kardia
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 	"testing"
 
-	"github.com/blendle/zapdriver"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/kardiachain/go-kardiamain/lib/common"
 )
 
-func SetupKAIClientForBenchmark() (*Client, context.Context, error) {
-	ctx, cancelFn := context.WithCancel(context.Background())
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		for range sigCh {
-			cancelFn()
-		}
-	}()
-	cfg := zapdriver.NewProductionConfig()
-	logger, err := cfg.Build()
-	if err != nil {
-		return nil, nil, fmt.Errorf("Failed to create logger: %v", err)
-	}
-	defer logger.Sync()
-	client, err := NewKaiClient("http://10.10.0.251:8551", logger)
-	if err != nil {
-		return nil, nil, fmt.Errorf("Failed to create new KaiClient: %v", err)
-	}
-	return client, ctx, nil
-}
-
 func BenchmarkLatestBlockNumber(b *testing.B) {
-	client, ctx, err := SetupKAIClientForBenchmark()
+	client, ctx, _, err := SetupKAIClient()
 	assert.Nil(b, err)
 
 	for i := 0; i < b.N; i++ {
@@ -64,58 +36,66 @@ func BenchmarkLatestBlockNumber(b *testing.B) {
 }
 
 func BenchmarkBlockByHash(b *testing.B) {
-	client, ctx, err := SetupKAIClientForBenchmark()
+	client, ctx, testSuite, err := SetupKAIClient()
 	assert.Nil(b, err)
-	hash := "0x63e5862cd056fc0807beb5d47a39b9eac5900c33673df78c1c216b0a3a3f4100"
 	for i := 0; i < b.N; i++ {
-		_, _ = client.BlockByHash(ctx, common.HexToHash(hash))
+		_, _ = client.BlockByHash(ctx, common.HexToHash(testSuite.blockHash))
 	}
 }
 
 func BenchmarkBlockByNumber(b *testing.B) {
-	client, ctx, err := SetupKAIClientForBenchmark()
+	client, ctx, testSuite, err := SetupKAIClient()
 	assert.Nil(b, err)
 	for i := 0; i < b.N; i++ {
-		_, _ = client.BlockByNumber(ctx, 50)
+		_, _ = client.BlockByNumber(ctx, testSuite.blockHeight)
 	}
 }
 
 func BenchmarkBlockHeaderByNumber(b *testing.B) {
-	client, ctx, err := SetupKAIClientForBenchmark()
+	client, ctx, testSuite, err := SetupKAIClient()
 	assert.Nil(b, err)
-
 	for i := 0; i < b.N; i++ {
-		_, _ = client.BlockHeaderByNumber(ctx, 50)
+		_, _ = client.BlockHeaderByNumber(ctx, testSuite.blockHeight)
 	}
 }
 
 func BenchmarkBlockHeaderByHash(b *testing.B) {
-	client, ctx, err := SetupKAIClientForBenchmark()
+	client, ctx, testSuite, err := SetupKAIClient()
 	assert.Nil(b, err)
-	hash := "0x63e5862cd056fc0807beb5d47a39b9eac5900c33673df78c1c216b0a3a3f4100"
-
 	for i := 0; i < b.N; i++ {
-		_, _ = client.BlockHeaderByHash(ctx, common.HexToHash(hash))
+		_, _ = client.BlockHeaderByHash(ctx, common.HexToHash(testSuite.blockHash))
 	}
 }
 
 func BenchmarkBalanceAt(b *testing.B) {
-	client, ctx, err := SetupKAIClientForBenchmark()
+	client, ctx, testSuite, err := SetupKAIClient()
 	assert.Nil(b, err)
-	addr := "0xfF3dac4f04dDbD24dE5D6039F90596F0a8bb08fd"
-
 	for i := 0; i < b.N; i++ {
-		_, _ = client.BalanceAt(ctx, common.HexToAddress(addr), common.NewZeroHash(), 0)
+		_, _ = client.BalanceAt(ctx, common.HexToAddress(testSuite.address), common.NewZeroHash(), 0)
 	}
 }
 
 func BenchmarkNonceAt(b *testing.B) {
-	client, ctx, err := SetupKAIClientForBenchmark()
+	client, ctx, testSuite, err := SetupKAIClient()
 	assert.Nil(b, err)
-	addr := "0xfF3dac4f04dDbD24dE5D6039F90596F0a8bb08fd"
-
 	for i := 0; i < b.N; i++ {
-		_, _ = client.NonceAt(ctx, common.HexToAddress(addr))
+		_, _ = client.NonceAt(ctx, common.HexToAddress(testSuite.address))
+	}
+}
+
+func BenchmarkGetTransaction(b *testing.B) {
+	client, ctx, testSuite, err := SetupKAIClient()
+	assert.Nil(b, err)
+	for i := 0; i < b.N; i++ {
+		_, _, _ = client.GetTransaction(ctx, common.HexToHash(testSuite.txHash))
+	}
+}
+
+func BenchmarkGetTransactionReceipt(b *testing.B) {
+	client, ctx, testSuite, err := SetupKAIClient()
+	assert.Nil(b, err)
+	for i := 0; i < b.N; i++ {
+		_, _ = client.GetTransactionReceipt(ctx, common.HexToHash(testSuite.txHash))
 	}
 }
 
--- types/transaction.go
+++ types/transaction.go
@@ -1,29 +1,26 @@
 package types
 
 import (
-	"math/big"
-
-	"github.com/kardiachain/go-kardiamain/lib/common"
 	"github.com/kardiachain/go-kardiamain/types"
 )
 
 type Transaction struct {
-	TxHash           string        `json:"hash" bson:"hash"`
-	To               string        `json:"to" bson:"to"`
-	From             string        `json:"from" bson:"from"`
-	Status           bool          `json:"status" bson:"status"`
-	ContractAddress  string        `json:"contract_address" bson:"contract_address"`
-	Value            string        `json:"value" bson:"value"`
-	GasPrice         string        `json:"gasPrice" bson:"gasPrice"`
-	GasFee           big.Int       `json:"gas" bson:"gas"`
-	GasLimit         common.Uint64 `json:"gasLimit" bson:"gasLimit"`
-	BlockNumber      uint64        `json:"blockNumber" bson:"blockNumber"`
-	Nonce            string        `json:"nonce" bson:"nonce"`
-	BlockHash        string        `json:"blockHash" bson:"blockHash"`
-	CreatedAt        uint64        `json:"time" bson:"time"`
-	InputData        string        `json:"input" bson:"input"`
-	Logs             string        `json:"logs" bson:"logs"`
-	TransactionIndex uint          `json:"transactionIndex"`
+	TxHash           string `json:"hash" bson:"hash"`
+	To               string `json:"to" bson:"to"`
+	From             string `json:"from" bson:"from"`
+	Status           bool   `json:"status" bson:"status"`
+	ContractAddress  string `json:"contract_address" bson:"contract_address"`
+	Value            string `json:"value" bson:"value"`
+	GasPrice         uint64 `json:"gasPrice" bson:"gasPrice"`
+	GasFee           uint64 `json:"gas" bson:"gas"`
+	GasLimit         uint64 `json:"gasLimit" bson:"gasLimit"`
+	BlockNumber      uint64 `json:"blockNumber" bson:"blockNumber"`
+	Nonce            uint64 `json:"nonce" bson:"nonce"`
+	BlockHash        string `json:"blockHash" bson:"blockHash"`
+	Time             uint64 `json:"time" bson:"time"`
+	InputData        string `json:"input" bson:"input"`
+	Logs             string `json:"logs" bson:"logs"`
+	TransactionIndex uint   `json:"transactionIndex"`
 
 	ReceiptReceived bool `json:"-" bson:"receipt_received"`
 }
@@ -41,19 +38,19 @@ type Log struct {
 }
 
 type Receipt struct {
-	BlockHash         string       `json:"blockHash"`
-	BlockHeight       uint64       `json:"blockHeight"`
-	TransactionHash   string       `json:"transactionHash"`
-	TransactionIndex  uint64       `json:"transactionIndex"`
-	From              string       `json:"from"`
-	To                string       `json:"to"`
-	GasUsed           uint64       `json:"gasUsed"`
-	CumulativeGasUsed uint64       `json:"cumulativeGasUsed"`
-	ContractAddress   string       `json:"contractAddress"`
-	Logs              []Log        `json:"logs"`
-	LogsBloom         types.Bloom  `json:"logsBloom"`
-	Root              common.Bytes `json:"root"`
-	Status            uint         `json:"status"`
+	BlockHash         string      `json:"blockHash"`
+	BlockHeight       uint64      `json:"blockHeight"`
+	TransactionHash   string      `json:"transactionHash"`
+	TransactionIndex  uint64      `json:"transactionIndex"`
+	From              string      `json:"from"`
+	To                string      `json:"to"`
+	GasUsed           uint64      `json:"gasUsed"`
+	CumulativeGasUsed uint64      `json:"cumulativeGasUsed"`
+	ContractAddress   string      `json:"contractAddress"`
+	Logs              []Log       `json:"logs"`
+	LogsBloom         types.Bloom `json:"logsBloom"`
+	Root              string      `json:"root"`
+	Status            uint        `json:"status"`
 }
 
 type TransactionList struct {
