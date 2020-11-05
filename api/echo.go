adb2c75ebd55ef140aa096206aca8419d44bbaa8
Long Nguyen <ngdlong91@gmail.com>
2020-11-05 18:21:41 +0700

    Update endpoint for token details
    Signed-off-by: Long Nguyen <ngdlong91@gmail.com>

--- api/echo.go
+++ api/echo.go
@@ -111,11 +111,15 @@ func bind(gr *echo.Group, srv EchoServer) {
 			fn:          srv.Search,
 			middlewares: []echo.MiddlewareFunc{checkPagination()},
 		},
-		// Dashboard
 		{
 			method: echo.GET,
 			path:   "/dashboard/stats",
 			fn:     srv.Stats,
+		},
+		{
+			method: echo.GET,
+			path:   "/dashboard/token",
+			fn:     srv.TokenInfo,
 		},
 		// Blocks
 		{
