a082cc8dcd1f15c18a7411a343498ca3be2cff4b
Long Nguyen <ngdlong91@gmail.com>
2020-12-29 15:32:38 +0700

    Clean code
    Signed-off-by: Long Nguyen <ngdlong91@gmail.com>

--- utils/kai.go
+++ utils/kai.go
@@ -18,33 +18,6 @@
 // Package utils
 package utils
 
-import (
-	"errors"
-	"strings"
-)
-
-func CleanUpHex(s string) string {
-	s = strings.Replace(strings.TrimPrefix(s, "0x"), " ", "", -1)
-	return strings.ToLower(s)
-}
-
-func ValidateAccount(accountAddress string) (string, error) {
-	accountAddress = CleanUpHex(accountAddress)
-	// check account length
-	if len(accountAddress) != 40 {
-		return "", errors.New("invalid account address")
-	}
-	return accountAddress, nil
-}
-
-func AppendNotEmpty(slice []string, str string) []string {
-	if str != "" {
-		return append(slice, str)
-	}
-
-	return slice
-}
-
 func IsNilAddress(address string) bool {
 	return address == "0x0000000000000000000000000000000000000000"
 }
