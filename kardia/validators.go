d049e08722a7803a76c96c8d26a8c5bdd6621474
Long Nguyen <ngdlong91@gmail.com>
2021-03-09 12:05:23 +0700

    Update list validator
    Signed-off-by: Long Nguyen <ngdlong91@gmail.com>

--- kardia/validators.go
+++ kardia/validators.go
@@ -4,6 +4,7 @@ package kardia
 import (
 	"context"
 	"fmt"
+	"math/big"
 	"time"
 
 	"github.com/kardiachain/go-kaiclient/kardia"
@@ -27,7 +28,28 @@ func (ec *Client) getValidators(ctx context.Context) ([]*types.Validator, error)
 	}
 	fmt.Println("TotalTime: ", time.Now().Sub(startLoadValidatorTime))
 	var validators []*types.Validator
+	totalStakedAmount := big.NewInt(0)
 	for _, v := range nValidators {
+		totalStakedAmount = new(big.Int).Add(totalStakedAmount, v.Tokens)
+	}
+	for _, v := range nValidators {
+		commissionRate, err := convertBigIntToPercentage(v.Commission.Rate.String())
+		if err != nil {
+			return nil, err
+		}
+		commissionMaxChangeRate, err := convertBigIntToPercentage(v.Commission.MaxChangeRate.String())
+		if err != nil {
+			return nil, err
+		}
+		commissionMaxRate, err := convertBigIntToPercentage(v.Commission.MaxRate.String())
+		if err != nil {
+			return nil, err
+		}
+		votingPowerPercent, err := calculateVotingPower(v.Tokens.String(), totalStakedAmount)
+		if err != nil {
+			return nil, err
+		}
+
 		validators = append(validators, &types.Validator{
 			Address:               v.Signer,
 			SmcAddress:            v.SMCAddress,
@@ -35,15 +57,21 @@ func (ec *Client) getValidators(ctx context.Context) ([]*types.Validator, error)
 			Role:                  0,
 			Jailed:                v.Jailed,
 			Name:                  validatorNameInString(v.Name),
-			VotingPowerPercentage: "",
-			StakedAmount:          "",
-			AccumulatedCommission: "",
-			UpdateTime:            0,
-			CommissionRate:        "",
-			TotalDelegators:       0,
-			MaxRate:               "",
-			MaxChangeRate:         "",
-			SigningInfo:           nil,
+			VotingPowerPercentage: votingPowerPercent,
+			StakedAmount:          v.Tokens.String(),
+			AccumulatedCommission: v.AccumulatedCommission.String(),
+			UpdateTime:            v.UpdateTime.Uint64(),
+			CommissionRate:        commissionRate,
+			MaxRate:               commissionMaxRate,
+			MaxChangeRate:         commissionMaxChangeRate,
+			SigningInfo: &types.SigningInfo{
+				StartHeight:        v.SigningInfo.StartHeight.Uint64(),
+				IndexOffset:        v.SigningInfo.IndexOffset.Uint64(),
+				Tombstoned:         v.SigningInfo.Tombstoned,
+				MissedBlockCounter: v.SigningInfo.MissedBlockCounter.Uint64(),
+				IndicatorRate:      100 - float64(v.SigningInfo.MissedBlockCounter.Uint64())/100,
+				JailedUntil:        v.SigningInfo.JailedUntil.Uint64(),
+			},
 		})
 	}
 	return validators, nil
