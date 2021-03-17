ff37b95c8f05dc91ef369fd9203ffe18ac9f3be8
Long Nguyen <ngdlong91@gmail.com>
2021-03-18 02:21:38 +0700

    Fix update proposer list
    Signed-off-by: Long Nguyen <ngdlong91@gmail.com>

--- db/validators_test.go
+++ db/validators_test.go
@@ -2,9 +2,15 @@
 package db
 
 import (
+	"context"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_mongoDB_UpdateProposers(t *testing.T) {
+	mgo, err := GetMgo()
+	assert.Nil(t, err)
 
+	assert.Nil(t, mgo.UpdateProposers(context.Background(), []string{"0x50a26DF56fC91eECF7f25D52eFB4eFAB56Dacf08"}))
 }
