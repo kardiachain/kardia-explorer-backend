17aa5b71c1aaf68e5540cde3de256a0f60e2f36c
AjackTi <dtrong97vn@gmail.com>
2021-04-20 16:45:49 +0700

    Merge code from dev

--- db/validators_test.go
+++ db/validators_test.go
@@ -14,10 +14,3 @@ func Test_mongoDB_UpdateProposers(t *testing.T) {
 
 	assert.Nil(t, mgo.UpdateProposers(context.Background(), []string{"0x50a26DF56fC91eECF7f25D52eFB4eFAB56Dacf08"}))
 }
-
-func Test_mongoDB_RemoveValidator(t *testing.T) {
-	mgo, err := GetMgo()
-	assert.Nil(t, err)
-
-	assert.Nil(t, mgo.RemoveValidator(context.Background(), "0x51C8820cA11bCFc6cFC4Ee1244aFF4a62c708ba3"))
-}
