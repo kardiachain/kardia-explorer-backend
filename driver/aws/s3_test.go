f8b36098868d431391a204d49902ed075c51986c
AjackTi <dtrong97vn@gmail.com>
2021-04-21 16:00:09 +0700

    Fix mistake input function

--- driver/aws/s3_test.go
+++ driver/aws/s3_test.go
@@ -1,17 +1,13 @@
 package aws
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestConnecttion_ConnectAws(t *testing.T) {
-	session, err := ConnectAws(Config{
-		KeyID:     "AKIAJI3Y5XWKQTDRL5HQ",
-		KeyAccess: "GWGuKvvVnUAQCGAmY937QcKkX//0RR2SPrdh+F3w",
-		Region:    aws.String("ap-southeast-1"),
-	})
+	s3 := &S3{}
+	session, err := s3.ConnectAws()
 	assert.Nil(t, err)
 	assert.NotNil(t, session)
 }
