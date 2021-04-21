453744e97e79351e20c9e2e0fa28c45091df1618
AjackTi <dtrong97vn@gmail.com>
2021-04-21 09:30:13 +0700

    Fix bug add new config to test file

--- driver/aws/s3_test.go
+++ driver/aws/s3_test.go
@@ -1,12 +1,17 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestConnecttion_ConnectAws(t *testing.T) {
-	session, err := ConnectAws()
+	session, err := ConnectAws(Config{
+		KeyID:     "AKIAJI3Y5XWKQTDRL5HQ",
+		KeyAccess: "GWGuKvvVnUAQCGAmY937QcKkX//0RR2SPrdh+F3w",
+		Region:    aws.String("ap-southeast-1"),
+	})
 	assert.Nil(t, err)
 	assert.NotNil(t, session)
 }
