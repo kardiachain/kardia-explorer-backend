54bfeec43aaebf55f0cc442deb1ef25cca0ddc2f
Long Nguyen <ngdlong91@gmail.com>
2021-12-24 16:06:24 +0700

    Force load proposal from chain
    Signed-off-by: Long Nguyen <ngdlong91@gmail.com>

--- server/api/proposal.go
+++ server/api/proposal.go
@@ -13,40 +13,40 @@ import (
 func (s *Server) GetProposalsList(c echo.Context) error {
 	ctx := context.Background()
 	pagination, page, limit := getPagingOption(c)
-	dbResult, dbTotal, dbErr := s.dbClient.GetListProposals(ctx, pagination)
-	if dbErr != nil {
-		return Invalid.Build(c)
-	}
+	//dbResult, dbTotal, dbErr := s.dbClient.GetListProposals(ctx, pagination)
+	//if dbErr != nil {
+	//	return Invalid.Build(c)
+	//}
 	rpcResult, rpcTotal, rpcErr := s.kaiClient.GetProposals(ctx, pagination)
 	if rpcErr != nil {
 		fmt.Println("GetProposals err: ", rpcErr)
 		return Invalid.Build(c)
 	}
-	if dbTotal != rpcTotal { // try to find out and insert missing proposals to db
-		isFound := false
-		for _, rpcProposal := range rpcResult {
-			isFound = false
-			for _, dbProposal := range dbResult {
-				if dbProposal.ID == rpcProposal.ID {
-					isFound = true
-					break
-				}
-			}
-			if isFound {
-				continue
-			}
-			dbResult = append(dbResult, rpcProposal) // include new proposal in response
-			s.logger.Info("Inserting new proposal", zap.Any("proposal", rpcProposal))
-			err := s.dbClient.UpsertProposal(ctx, rpcProposal) // insert missing proposal to db
-			if err != nil {
-				s.logger.Debug("Cannot insert new proposal to DB", zap.Error(err))
-			}
-		}
-	}
+	//if dbTotal != rpcTotal { // try to find out and insert missing proposals to db
+	//	isFound := false
+	//	for _, rpcProposal := range rpcResult {
+	//		isFound = false
+	//		for _, dbProposal := range dbResult {
+	//			if dbProposal.ID == rpcProposal.ID {
+	//				isFound = true
+	//				break
+	//			}
+	//		}
+	//		if isFound {
+	//			continue
+	//		}
+	//		dbResult = append(dbResult, rpcProposal) // include new proposal in response
+	//		s.logger.Info("Inserting new proposal", zap.Any("proposal", rpcProposal))
+	//		err := s.dbClient.UpsertProposal(ctx, rpcProposal) // insert missing proposal to db
+	//		if err != nil {
+	//			s.logger.Debug("Cannot insert new proposal to DB", zap.Error(err))
+	//		}
+	//	}
+	//}
 	return OK.SetData(PagingResponse{
 		Page:  page,
 		Limit: limit,
-		Data:  dbResult,
+		Data:  rpcResult,
 		Total: rpcTotal,
 	}).Build(c)
 }
